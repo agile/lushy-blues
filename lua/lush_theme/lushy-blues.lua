84da4fa33d643a21d022d100797560bff8292eff
Mike Vincent <mike@vincent.ws>
2022-10-22 13:09:01 -0500

    Starting from the old brookstream scheme

--- lua/lush_theme/lushy-blues.lua
+++ lua/lush_theme/lushy-blues.lua
@@ -45,6 +45,29 @@
 local lush = require('lush')
 local hsl = lush.hsl
 
+local black = hsl(0, 0, 0)              -- #000000
+local nearly_blacker = hsl(0, 0, 2)     -- #050505
+local nearly_black = hsl(0, 0, 3)       -- #080808
+local darker_gray = hsl(0, 0, 27)       -- #454545
+local dark_gray = hsl(0, 0, 50)         -- #808080
+local mid_red = hsl(0,100,27)           -- #880000
+local bright_red = hsl(0,100,37)        -- #BD0000
+local steal_blue = hsl(180, 25, 25)     -- #2F4F4F
+local dark_blue = hsl(240, 100, 27)     -- #000088
+local royal_blue = hsl(240, 100, 63)    -- #4444FF
+local steal_teal = hsl(207,44,49)       -- #4682b4
+local mid_teal = hsl(180,100,63)        -- #44FFFF
+local gray_blue = hsl(180, 14, 77)      -- #BBCCCC
+local white = hsl(0, 0, 100)            -- #FFFFFF
+
+local bright_green = hsl(120, 100, 63)  -- #44FF44
+local bright_yellow = hsl(60, 100, 50)  -- #FFFF00
+
+local sea_foam  = hsl(208, 80, 80)      -- Try presing C-a and C-x
+local sea_crest = hsl(208, 90, 30)      -- to increment or decrement
+local sea_deep  = hsl(208, 90, 10)      -- the integers used here.
+local sea_sand  = hsl(0, 0, 73)         -- #BBBBBB
+
 -- LSP/Linters mistakenly show `undefined global` errors in the spec, they may
 -- support an annotation like the following. Consult your server documentation.
 ---@diagnostic disable: undefined-global
@@ -58,62 +81,79 @@ local theme = lush(function()
     -- To be able to link to a group, it must already be defined, so you may have
     -- to reorder items as you go.
     --
+    --   <HighlightGroupName> { bg = <hsl>|<string>, -- background
+    --                          fg = <hsl>|<string>, -- foreground
+    --                          sp = <hsl>|<string>, -- special character
+    --                          gui = <string> },    -- bold, underline, undercurl, etc (:he attr-list)
+    --
+    --   Relative adjustment (rotate(), saturate(), desaturate(), lighten(), darken())
+    --   Absolute adjustment (prefix above with abs_)
+    --   Combination         (mix())
+    --   Overrides           (hue(), saturation(), lightness())
+    --   Access              (.h, .s, .l)
+    --   Coercion            (tostring(), "Concatination: " .. color)
+    --   Helpers             (readable())
+    --
+    --   Adjustment functions have shortcut aliases, ro, sa, de, li, da
+    --                                               abs_sa, abs_de, abs_li, abs_da
+    --
     -- See :h highlight-groups
     --
     -- ColorColumn  { }, -- Columns set with 'colorcolumn'
-    -- Conceal      { }, -- Placeholder characters substituted for concealed text (see 'conceallevel')
-    -- Cursor       { }, -- Character under the cursor
+    -- Conceal         { fg = Normal.fg.da(20), gui="italic" }, -- Placeholder characters substituted for concealed text (see 'conceallevel')
+    Cursor          { fg = black, bg = bright_green }, -- Character under the cursor
     -- lCursor      { }, -- Character under the cursor when |language-mapping| is used (see 'guicursor')
     -- CursorIM     { }, -- Like Cursor, but used when in IME mode |CursorIM|
     -- CursorColumn { }, -- Screen-column at the cursor, when 'cursorcolumn' is set.
     -- CursorLine   { }, -- Screen-line at the cursor, when 'cursorline' is set. Low-priority if foreground (ctermfg OR guifg) is not set.
-    -- Directory    { }, -- Directory names (and other special names in listings)
-    -- DiffAdd      { }, -- Diff mode: Added line |diff.txt|
-    -- DiffChange   { }, -- Diff mode: Changed line |diff.txt|
-    -- DiffDelete   { }, -- Diff mode: Deleted line |diff.txt|
-    -- DiffText     { }, -- Diff mode: Changed text within a changed line |diff.txt|
+    Directory       { fg=mid_teal }, -- Directory names (and other special names in listings)
+    DiffAdd         { fg=bright_yellow, bg=nearly_black }, -- |diff.txt|
+    DiffChange      { fg=white, bg=nearly_black }, -- Diff mode: Changed line |diff.txt|
+    DiffDelete      { fg = darker_gray, bg = nearly_black }, -- Diff mode: Deleted line |diff.txt|
+    DiffText        { fg = mid_red, bg = nearly_black }, -- Diff mode: Changed text within a changed line |diff.txt|
     -- EndOfBuffer  { }, -- Filler lines (~) after the end of the buffer. By default, this is highlighted like |hl-NonText|.
     -- TermCursor   { }, -- Cursor in a focused terminal
     -- TermCursorNC { }, -- Cursor in an unfocused terminal
-    -- ErrorMsg     { }, -- Error messages on the command line
+    ErrorMsg        { fg = white, bg = mid_red }, -- Error messages on the command line
     -- VertSplit    { }, -- Column separating vertically split windows
-    -- Folded       { }, -- Line used for closed folds
+    Folded          { fg = dark_blue }, -- Line used for closed folds
     -- FoldColumn   { }, -- 'foldcolumn'
     -- SignColumn   { }, -- Column where |signs| are displayed
-    -- IncSearch    { }, -- 'incsearch' highlighting; also used for the text replaced with ":s///c"
+    IncSearch       { fg = gray_blue, bg = black }, -- 'incsearch' highlighting; also used for the text replaced with ":s///c"
     -- Substitute   { }, -- |:substitute| replacement text highlighting
-    -- LineNr       { }, -- Line number for ":number" and ":#" commands, and when 'number' or 'relativenumber' option is set.
+    LineNr          { fg = steal_teal, bg = nearly_blacker }, -- Line number for ":number" and ":#" commands, and when 'number' or 'relativenumber' option is set.
     -- CursorLineNr { }, -- Like LineNr when 'cursorline' or 'relativenumber' is set for the cursor line.
     -- MatchParen   { }, -- Character under the cursor or just before it, if it is a paired bracket, and its match. |pi_paren.txt|
-    -- ModeMsg      { }, -- 'showmode' message (e.g., "-- INSERT -- ")
+    ModeMsg         { fg = white }, -- 'showmode' message (e.g., "-- INSERT -- ")
     -- MsgArea      { }, -- Area for messages and cmdline
     -- MsgSeparator { }, -- Separator for scrolled messages, `msgsep` flag of 'display'
-    -- MoreMsg      { }, -- |more-prompt|
-    -- NonText      { }, -- '@' at the end of the window, characters from 'showbreak' and other characters that do not really exist in the text (e.g., ">" displayed when a double-wide character doesn't fit at the end of the line). See also |hl-EndOfBuffer|.
-    -- Normal       { }, -- Normal text
+    MoreMsg         { fg = bright_green }, -- |more-prompt|
+    NonText         { fg = royal_blue }, -- '@' at the end of the window, characters from 'showbreak' and other characters that do not really exist in the text (e.g., ">" displayed when a double-wide character doesn't fit at the end of the line). See also |hl-EndOfBuffer|.
+    Normal          { fg = sea_sand, bg = black  }, -- Normal text
     -- NormalFloat  { }, -- Normal text in floating windows.
     -- NormalNC     { }, -- normal text in non-current windows
-    -- Pmenu        { }, -- Popup menu: Normal item.
-    -- PmenuSel     { }, -- Popup menu: Selected item.
+    Pmenu           { fg=dark_blue, bg=mid_teal }, -- Popup menu: Normal item.
+    PmenuSel        { fg = mid_teal, bg = dark_blue, gui="underline" }, -- Popup menu: Selected item.
     -- PmenuSbar    { }, -- Popup menu: Scrollbar.
     -- PmenuThumb   { }, -- Popup menu: Thumb of the scrollbar.
-    -- Question     { }, -- |hit-enter| prompt and yes/no questions
+
+    Question        { fg = bright_yellow }, -- |hit-enter| prompt and yes/no questions
     -- QuickFixLine { }, -- Current |quickfix| item in the quickfix window. Combined with |hl-CursorLine| when the cursor is there.
     -- Search       { }, -- Last search pattern highlighting (see 'hlsearch'). Also used for similar items that need to stand out.
-    -- SpecialKey   { }, -- Unprintable characters: text displayed differently from what it really is. But not 'listchars' whitespace. |hl-Whitespace|
+    SpecialKey      { fg = royal_blue }, -- Unprintable characters: text displayed differently from what it really is. But not 'listchars' whitespace. |hl-Whitespace|
     -- SpellBad     { }, -- Word that is not recognized by the spellchecker. |spell| Combined with the highlighting used otherwise.
     -- SpellCap     { }, -- Word that should start with a capital. |spell| Combined with the highlighting used otherwise.
     -- SpellLocal   { }, -- Word that is recognized by the spellchecker as one that is used in another region. |spell| Combined with the highlighting used otherwise.
     -- SpellRare    { }, -- Word that is recognized by the spellchecker as one that is hardly ever used. |spell| Combined with the highlighting used otherwise.
-    -- StatusLine   { }, -- Status line of current window
-    -- StatusLineNC { }, -- Status lines of not-current windows. Note: If this is equal to "StatusLine" Vim will use "^^^" in the status line of the current window.
+    StatusLine      { fg = white, bg = steal_blue }, -- Status line of current window
+    StatusLineNC    { fg = black, bg = sea_sand }, -- Status lines of not-current windows. Note: If this is equal to "StatusLine" Vim will use "^^^" in the status line of the current window.
     -- TabLine      { }, -- Tab pages line, not active tab page label
     -- TabLineFill  { }, -- Tab pages line, where there are no labels
     -- TabLineSel   { }, -- Tab pages line, active tab page label
-    -- Title        { }, -- Titles for output from ":set all", ":autocmd" etc.
-    -- Visual       { }, -- Visual mode selection
+    Title           { fg = white }, -- Titles for output from ":set all", ":autocmd" etc.
+    Visual          { fg = black, bg = sea_sand }, -- Visual mode selection
     -- VisualNOS    { }, -- Visual mode selection when vim is "Not Owning the Selection".
-    -- WarningMsg   { }, -- Warning messages
+    WarningMsg      { fg = bright_yellow }, -- Warning messages
     -- Whitespace   { }, -- "nbsp", "space", "tab" and "trail" in 'listchars'
     -- Winseparator { }, -- Separator between window splits. Inherts from |hl-VertSplit| by default, which it will replace eventually.
     -- WildMenu     { }, -- Current match in 'wildmenu' completion
@@ -126,48 +166,48 @@ local theme = lush(function()
     --
     -- Uncomment and edit if you want more specific syntax highlighting.
 
-    -- Comment        { }, -- Any comment
+    Comment           { fg = Normal.fg.darken(60) }, -- Any comment
 
-    -- Constant       { }, -- (*) Any constant
-    -- String         { }, --   A string constant: "this is a string"
+    Constant          { fg = hsl("#00aaaa") }, -- (*) Any constant
+    String            { fg = hsl("#4682B4"), gui="none" }, --   A string constant: "this is a string"
     -- Character      { }, --   A character constant: 'c', '\n'
     -- Number         { }, --   A number constant: 234, 0xff
-    -- Boolean        { }, --   A boolean constant: TRUE, false
+    Boolean           { fg = hsl("#9BCD9B") }, --   A boolean constant: TRUE, false
     -- Float          { }, --   A floating point constant: 2.3e10
 
-    -- Identifier     { }, -- (*) Any variable name
-    -- Function       { }, --   Function name (also: methods for classes)
+    Identifier        { fg = hsl("#00E5EE") }, -- (*) Any variable name
+    Function          { fg = hsl("#1E90FF") }, --   Function name (also: methods for classes)
 
-    -- Statement      { }, -- (*) Any statement
+    Statement         { fg = hsl("#00FFFF") }, -- (*) Any statement
     -- Conditional    { }, --   if, then, else, endif, switch, etc.
     -- Repeat         { }, --   for, do, while, etc.
     -- Label          { }, --   case, default, etc.
-    -- Operator       { }, --   "sizeof", "+", "*", etc.
+    Operator          { fg = hsl("#00BFFF"), gui="none" }, --   "sizeof", "+", "*", etc.
     -- Keyword        { }, --   any other keyword
     -- Exception      { }, --   try, catch, throw
 
-    -- PreProc        { }, -- (*) Generic Preprocessor
+    PreProc           { fg = hsl("#8470FF") }, -- (*) Generic Preprocessor
     -- Include        { }, --   Preprocessor #include
     -- Define         { }, --   Preprocessor #define
     -- Macro          { }, --   Same as Define
     -- PreCondit      { }, --   Preprocessor #if, #else, #endif, etc.
 
-    -- Type           { }, -- (*) int, long, char, etc.
+    Type              { fg = white }, -- (*) int, long, char, etc.
     -- StorageClass   { }, --   static, register, volatile, etc.
     -- Structure      { }, --   struct, union, enum, etc.
     -- Typedef        { }, --   A typedef
 
-    -- Special        { }, -- (*) Any special symbol
+    Special           { fg = hsl("#87CEFA"), gui="none" }, -- (*) Any special symbol
     -- SpecialChar    { }, --   Special character in a constant
     -- Tag            { }, --   You can use CTRL-] on this
     -- Delimiter      { }, --   Character that needs attention
     -- SpecialComment { }, --   Special things inside a comment (e.g. '\n')
     -- Debug          { }, --   Debugging statements
 
-    -- Underlined     { gui = "underline" }, -- Text that stands out, HTML links
-    -- Ignore         { }, -- Left blank, hidden |hl-Ignore| (NOTE: May be invisible here in template)
-    -- Error          { }, -- Any erroneous construct
-    -- Todo           { }, -- Anything that needs extra attention; mostly the keywords TODO FIXME and XXX
+    Underlined        { fg = hsl("#4444FF"), gui = "bold" }, -- gui = "underline" }, -- Text that stands out, HTML links
+    Ignore            { fg = hsl("#444444") }, -- Left blank, hidden |hl-Ignore| (NOTE: May be invisible here in template)
+    Error             { fg = hsl("#BB0000"), bg = black }, -- Any erroneous construct
+    Todo              { fg = hsl("#FFF300"), bg = hsl("#AA0006") }, -- Anything that needs extra attention; mostly the keywords TODO FIXME and XXX
 
     -- These groups are for the native LSP client and diagnostic system. Some
     -- other LSP clients may use these groups, or use their own. Consult your
